ff76e601bc43563125b85b7a9f1c439b0a558f2a
Jamie <phillipsja97@gmail.com>
2020-03-21 10:58:10 -0500

    family members added

--- FamilyDictionary/Program.cs
+++ FamilyDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FamilyDictionary
 {
@@ -6,7 +7,20 @@ namespace FamilyDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+            myFamily.Add("Mother", new Dictionary<string, string>() { { "name", "Patricia" }, { "age", "71" } });
+
+            myFamily.Add("Father", new Dictionary<string, string>() { { "name", "Laurence" }, { "age", "69" } });
+
+            myFamily.Add("Sister", new Dictionary<string, string>() { { "name", "Josi" }, { "age", "36" } });
+
+            myFamily.Add("Wife", new Dictionary<string, string>() { { "name", "Bridget" }, { "age", "30" } });
+
+            foreach (var member in myFamily)
+            {
+                Console.WriteLine($"{member.Value["name"]} is my { member.Key } and is {member.Value["age"]} years old.");
+            }
+            Console.ReadLine();
         }
     }
 }
